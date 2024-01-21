4b2b0a6a9cb4b3ab2156745ba61790803a1fb30f
ethanellison <e_21997@hotmail.com>
2024-01-21 00:13:28 +0000

    deleted dbt-env folder and added ARGS to Dockerfile

--- P1/jupyter/script.py
+++ P1/jupyter/script.py
@@ -56,24 +56,13 @@ def process_to_df(url, match_date):
 items = [
     {
         "url": "https://data04.perfbook.pro/scripts/download.php?id=JjIwMjQwMTEyXzEzODI1X2RhbGhvdXNlLXNoZXJicm9va2VfMC0zLmR2d3x8aHR0cHM6Ly9kYXRhMDQucGVyZmJvb2sucHJvL19EQVRBL3ZvbGxleWJhbGwvMjAyNC9maWxlXzI1OGMwYjgxNmI0ZWMwNjUxNTcyMTU0MzMzM2RmZGUzMTgyNTA0N2I5Y2I0NDAyMC5kdnc=",
-        "match_date": "2024-01-12",
-    },
-    {
-        "url": "https://data04.perfbook.pro/scripts/download.php?id=JjEzODMzX01MQVZBTC1NVU5CIFJFRFMuZHZ3fHxodHRwczovL2RhdGEwNC5wZXJmYm9vay5wcm8vX0RBVEEvdm9sbGV5YmFsbC8yMDI0L2ZpbGVfMThjMjA4YmM2NWQ1NTQ2YWE5NGI4MWRlZmUwOWZhMGIwYWUwZjY4NTljYjQwMDI4LmR2dw==",
-        "match_date": "2024-01-12",
-    },
-    {
-        "url": "https://data04.perfbook.pro/scripts/download.php?id=JjEzODUzIE1EQUxIT1VJU0UtTVNIRVJCUk9PS0UgMC5kdnd8fGh0dHBzOi8vZGF0YTA0LnBlcmZib29rLnByby9fREFUQS92b2xsZXliYWxsLzIwMjQvZmlsZV9lNThkMDMxZGI5OTY4YzEzZTI1YmQ0ZmM3YTc4NjM1N2Q0MzNhYzMwOTRiZjM0NTMuZHZ3",
-        "match_date": "2024-01-13",
-    },
-    {
-        "url": "https://data04.perfbook.pro/scripts/download.php?id=JjIwMjQwMTEzXzEzODM3X2xhdmFsIHJvdWdlIG0tdW5iXzAtMy5kdnd8fGh0dHBzOi8vZGF0YTA0LnBlcmZib29rLnByby9fREFUQS92b2xsZXliYWxsLzIwMjQvZmlsZV84NjVlMzQ2NGNhOTg3ZDdkOTlmNGMwNGQ5NzUwMTRkNWRkNjFmODFlOTRiZjI4NDguZHZ3",
-        "match_date": "2024-01-13",
+        "match_date": "2024-01-19",
     },
 ]
 
 for item in items:
     output = process_to_df(item["url"], item["match_date"])
+    print(output[0], output[1])
     print(
         object_storage.put_object(
             namespace_name=namespace,
